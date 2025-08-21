8bb1358b9bf79e560397235b8f14582766ba5ba3
anand <anand24bala@gmail.com>
2025-08-22 01:05:51 +0800

    Personalised chatbot and updated .gitignore

---
+++ src/main/java/Dennis.java
@@ -0,0 +1,15 @@
+public class Dennis {
+    public static void main(String[] args) {
+        String logo = "██████╗ ███████╗███╗   ██╗███╗   ██╗██╗███████╗\n"
+                + "██╔══██╗██╔════╝████╗  ██║████╗  ██║██║██╔════╝\n"
+                + "██║  ██║█████╗  ██╔██╗ ██║██╔██╗ ██║██║███████╗\n"
+                + "██║  ██║██╔══╝  ██║╚██╗██║██║╚██╗██║██║╚════██║\n"
+                + "██████╔╝███████╗██║ ╚████║██║ ╚████║██║███████║\n"
+                + "╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝╚══════╝\n";
+        String indentation = "_______________________________________\n";
+        String message = indentation
+                + "this is DENNIS. What can I do for you?\n" + indentation
+                + "See ya later. Bye";
+        System.out.println(logo + message);
+    }
+}
