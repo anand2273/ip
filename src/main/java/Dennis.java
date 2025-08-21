603cdfd58f7f34adbae6c441cec14be0c155399d
anand <anand24bala@gmail.com>
2025-08-22 01:17:16 +0800

    Added echo and bye features to DENNIS

--- src/main/java/Dennis.java
+++ src/main/java/Dennis.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Dennis {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "██████╗ ███████╗███╗   ██╗███╗   ██╗██╗███████╗\n"
-                + "██╔══██╗██╔════╝████╗  ██║████╗  ██║██║██╔════╝\n"
-                + "██║  ██║█████╗  ██╔██╗ ██║██╔██╗ ██║██║███████╗\n"
-                + "██║  ██║██╔══╝  ██║╚██╗██║██║╚██╗██║██║╚════██║\n"
-                + "██████╔╝███████╗██║ ╚████║██║ ╚████║██║███████║\n"
-                + "╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝╚══════╝\n";
+                    + "██╔══██╗██╔════╝████╗  ██║████╗  ██║██║██╔════╝\n"
+                    + "██║  ██║█████╗  ██╔██╗ ██║██╔██╗ ██║██║███████╗\n"
+                    + "██║  ██║██╔══╝  ██║╚██╗██║██║╚██╗██║██║╚════██║\n"
+                    + "██████╔╝███████╗██║ ╚████║██║ ╚████║██║███████║\n"
+                    + "╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝╚══════╝\n";
         String indentation = "_______________________________________\n";
-        String message = indentation
-                + "this is DENNIS. What can I do for you?\n" + indentation
-                + "See ya later. Bye";
+        String message = (indentation
+                            + "this is DENNIS. What can I do for you?\n"
+                            + indentation);
         System.out.println(logo + message);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(indentation + "Catch you later!\n" + indentation);
+                break;
+            } else {
+                System.out.println(indentation + input + "\n" + indentation);
+            }
+        }
+        sc.close();
     }
 }
