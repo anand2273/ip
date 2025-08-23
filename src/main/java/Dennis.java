d5fe69dac76fc9fcd40f208a8729df2485cc623d
anand <anand24bala@gmail.com>
2025-08-23 18:18:10 +0800

    Exception and invalid input handling

--- src/main/java/Dennis.java
+++ src/main/java/Dennis.java
@@ -32,31 +32,51 @@ public class Dennis {
                     System.out.println((Ui.wrapText(taskList.toString())));
                     break;
                 case "mark":
-                    idx = Integer.parseInt(parts[1]) - 1;
-                    System.out.println(Ui.wrapText(taskList.get(idx).markDone()));
-                    break;
                 case "unmark":
-                    idx = Integer.parseInt(parts[1]) - 1;
-                    System.out.println(Ui.wrapText(taskList.get(idx).markUndone()));
+                    try {
+                        idx = Integer.parseInt(parts[1]) - 1;
+                        Task task = taskList.get(idx);
+                        String result = cmd.equals("mark") ? task.markDone() : task.markUndone();
+                        System.out.println(Ui.wrapText(result));
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println(Ui.wrapText("Please specify the task number in the following format: 'mark 1'"));
+                    } catch (NumberFormatException nfe) {
+                        System.out.println(Ui.wrapText("Please specify a numerical value for the task number in the following format: 'mark 1'"));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(Ui.wrapText("That task number does not exist. Please use the list command for task numbers."));
+                    }
                     break;
                 case "todo":
-                    String taskName = parts[1];
-                    TodoTask todoTask = new TodoTask(taskName);
-                    String todoMsg = taskList.add(todoTask);
-                    System.out.println(Ui.wrapText(todoMsg));
+                    try {
+                        String taskName = parts[1];
+                        TodoTask todoTask = new TodoTask(taskName);
+                        String todoMsg = taskList.add(todoTask);
+                        System.out.println(Ui.wrapText(todoMsg));
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Please specify a task.");
+                    }
                     break;
                 case "deadline":
-                    String[] remainder = parts[1].split("/by");
-                    DeadlineTask deadlineTask = new DeadlineTask(remainder[0].trim(), remainder[1].trim());
-                    String deadlineMsg = taskList.add(deadlineTask);
-                    System.out.println(Ui.wrapText(deadlineMsg));
+                    try {
+                        String[] remainder = parts[1].split("/by");
+                        DeadlineTask deadlineTask = new DeadlineTask(remainder[0].trim(), remainder[1].trim());
+                        String deadlineMsg = taskList.add(deadlineTask);
+                        System.out.println(Ui.wrapText(deadlineMsg));
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Invalid input. Please follow the prescribed format.");
+                    }
                     break;
                 case "event":
-                    String[] rest = parts[1].split("/from");
-                    String[] timeline = rest[1].split("/to");
-                    EventTask eventTask = new EventTask(rest[0].trim(), timeline[0].trim(), timeline[1].trim());
-                    String eventMsg = taskList.add(eventTask);
-                    System.out.println(Ui.wrapText(eventMsg));
+                    try {
+                        String[] rest = parts[1].split("/from");
+                        String[] timeline = rest[1].split("/to");
+                        EventTask eventTask = new EventTask(rest[0].trim(), timeline[0].trim(), timeline[1].trim());
+                        String eventMsg = taskList.add(eventTask);
+                        System.out.println(Ui.wrapText(eventMsg));
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Invalid input. Please follow the prescribed format.");
+                    }
                     break;
                 default:
                     Task task = new Task(cmd);
---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,9 @@
+//package parser;
+//import tasks.Task;
+//
+//public class Parser {
+//    public static Task parseTask(String message) {
+//
+//    }
+//
+//}
