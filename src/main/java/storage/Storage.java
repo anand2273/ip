04d0f8490a06962db022d9debee9d47a7c17340d
anand <anand24bala@gmail.com>
2025-08-25 23:26:15 +0800

    Add function to save task list to disk

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,10 +1,13 @@
 package storage;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import tasks.DeadlineTask;
 import tasks.TaskList;
 import tasks.Task;
+import tasks.TodoTask;
 
 public class Storage {
     private String filePath;
@@ -13,12 +16,33 @@ public class Storage {
         this.filePath = filePath;
     }
 
-    public void save(ArrayList<Task> tasks) {
-        return; // implement later
+    public void save(TaskList tasks) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        String textToAdd = "";
+        for (Task t : tasks.getTasks()) {
+            textToAdd += t.toStorage() + System.lineSeparator();
+        }
+        fw.write(textToAdd);
+        fw.close();
     }
 
     public void load() {
         return; // implement this later
+    }
+
+    private ArrayList<Task> parseTask(String storedTasks) {
+        return null; // implement this later
+    }
+
+    public static void main(String[] args) throws IOException {
+        Task task1 = new TodoTask("Eat");
+        Task task2 = new DeadlineTask("eat", "tomorrow");
+        TaskList taskList = new TaskList();
+        taskList.add(task1);
+        taskList.add(task2);
+        Storage storage = new Storage("data/dennis.txt");
+        storage.save(taskList);
+        System.out.println("done");
     }
 
 
