5c9f4b8c0caf0105c02ce234a4e9012ab689200f
anand <anand24bala@gmail.com>
2025-08-27 17:05:54 +0800

    Add UI class to deal with user-facing logic

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,9 +1,37 @@
 package ui;
+import tasks.TaskList;
+import tasks.Task;
 
 public class Ui {
     public static final String INDENTATION = "_______________________________________\n";
 
+    private static final String LOGO = " █████╗ ██╗     ███████╗██████╗ ███████╗██████╗\n" +
+                                       "██╔══██╗██║     ██╔════╝██╔══██╗██╔════╝██╔══██╗\n" +
+                                       "███████║██║     █████╗  ██████╔╝█████╗  ██║  ██║\n" +
+                                       "██╔══██║██║     ██╔══╝  ██╔══██╗██╔══╝  ██║  ██║\n" +
+                                       "██║  ██║███████╗██║     ██║  ██║███████╗██████╔╝\n" +
+                                       "╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═════╝";
     public static String wrapText(String msg) {
         return INDENTATION + msg + "\n" + INDENTATION;
     }
+
+    public void showWelcome() {
+        String message = LOGO + "Good day, Master Bruce. How may I assist you?";
+        System.out.println(Ui.wrapText(message));
+    }
+
+    public void showList(TaskList tasks) {
+        System.out.println(Ui.wrapText(tasks.toString()));
+    }
+
+    public void showMarked(Task task) {
+        String message = "Task has been marked as done.";
+        System.out.println(message + task.toString());
+    }
+
+    public void showUnmarked(Task task) {
+        String message = "Task has been marked as undone";
+        System.out.println(message + task.toString());
+    }
+
 }
