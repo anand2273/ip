fd613eaaa48271342400ceed12fcac79f02ff51f
anand <anand24bala@gmail.com>
2025-08-27 18:23:05 +0800

    Create UI class with functions to produce user-facing output

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -5,33 +5,54 @@ import tasks.Task;
 public class Ui {
     public static final String INDENTATION = "_______________________________________\n";
 
-    private static final String LOGO = " █████╗ ██╗     ███████╗██████╗ ███████╗██████╗\n" +
+    private static final String LOGO = "\n █████╗ ██╗     ███████╗██████╗ ███████╗██████╗\n" +
                                        "██╔══██╗██║     ██╔════╝██╔══██╗██╔════╝██╔══██╗\n" +
                                        "███████║██║     █████╗  ██████╔╝█████╗  ██║  ██║\n" +
                                        "██╔══██║██║     ██╔══╝  ██╔══██╗██╔══╝  ██║  ██║\n" +
                                        "██║  ██║███████╗██║     ██║  ██║███████╗██████╔╝\n" +
-                                       "╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═════╝";
+                                       "╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═════╝\n";
     public static String wrapText(String msg) {
         return INDENTATION + msg + "\n" + INDENTATION;
     }
 
     public void showWelcome() {
         String message = LOGO + "Good day, Master Bruce. How may I assist you?";
-        System.out.println(Ui.wrapText(message));
+        System.out.println(wrapText(message));
     }
 
     public void showList(TaskList tasks) {
-        System.out.println(Ui.wrapText(tasks.toString()));
+        System.out.println(wrapText(tasks.toString()));
     }
 
-    public void showMarked(Task task) {
-        String message = "Task has been marked as done.";
-        System.out.println(message + task.toString());
+    public void showMarked(Task task, boolean marked) {
+        String message = marked
+                        ? "Task has been marked as done, Master Bruce.\n"
+                        : "Task has already been marked as done, Master Bruce.\n";
+        message += task.toString();
+        System.out.println(wrapText(message));
     }
 
-    public void showUnmarked(Task task) {
-        String message = "Task has been marked as undone";
-        System.out.println(message + task.toString());
+    public void showUnmarked(Task task, boolean marked) {
+        String message = marked
+                ? "Task has been marked as undone, Master Bruce.\n"
+                : "Task has already been marked as undone, Master Bruce.\n";
+        message += task.toString();
+        System.out.println(wrapText(message));
+    }
+
+    public void showAddedTask(Task task) {
+        String message = "This task has been successfully added:\n" + task;
+        System.out.println(wrapText(message));
+    }
+
+    public void showDelete(TaskList tasks, Task task) {
+        String message = "Noted, Master Bruce. This task has been successfully deleted:\n" + task + "\n";
+        message += "There are " + tasks.size() + " tasks left.";
+        System.out.println(Ui.wrapText(message));
+    }
+
+    public void showDefault() {
+        System.out.println("Good Evening, Master Bruce. Please use the commands to continue.");
     }
 
 }
