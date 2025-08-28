540ccbc7f0f2233022b624d8d0498360066532ef
anand <anand24bala@gmail.com>
2025-08-28 17:41:00 +0800

    Add gradle support and automation

--- src/main/java/command/ExitCommand.java
+++ src/main/java/command/ExitCommand.java
@@ -9,4 +9,9 @@ public class ExitCommand extends Command {
     public void execute(TaskList tasks, Ui ui) {
         ui.showBye();
     }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
 }
