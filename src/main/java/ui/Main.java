f96717741d300d40255864f6b2b2e8fab634fe61
anand <anand24bala@gmail.com>
2025-09-01 17:58:50 +0800

    GUI integration using JavaFX

---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,29 @@
+package ui;
+import alfred.Alfred;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+    private Alfred alfred = new Alfred("data/alfred.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setAlfred(alfred);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
