662a330878f8c6d5eb5e02adcfd1b1d05c99c51b
anand <anand24bala@gmail.com>
2025-09-11 13:38:22 +0800

    Edit return message in sort command

--- src/main/java/command/SortCommand.java
+++ src/main/java/command/SortCommand.java
@@ -4,6 +4,6 @@ public class SortCommand extends Command {
     @Override
     public String execute(task.TaskList tasks, ui.Ui ui) {
         tasks.sortTasks();
-        return "Tasks have been sorted by description.";
+        return "Tasks have been sorted by date.";
     }
 }
